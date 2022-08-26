073aff49a5f1be514d4c0ee03648dcc099510de9
PrachitoshPradhan <prachitoshpp.pradhans@gmail.com>
2022-08-26 10:44:29 +0530

    ADDED: Fibonacci series code java

---
+++ src/FibonacciSeries/FibonacciNumbers.java
@@ -0,0 +1,24 @@
+package FibonacciSeries;
+import java.util.Scanner;
+
+public class FibonacciNumbers {
+	public static void main(String[] args) {
+     Scanner scanner = new Scanner(System.in);
+     System.out.println("Enter the number to print fibonacci series -  ");
+     int num = scanner.nextInt();
+     int num1=0;
+     int num2=1;
+     System.out.println(num1);
+     System.out.println(num2);
+     while(num>2)
+     {
+    	 int sum = num1+num2;
+    	 System.out.println(sum);
+    	 num1=num2;
+    	 num2=sum;
+    	 num--;
+     }
+     scanner.close();
+	}
+
+}
